ff97eacf586876363b8888742b636301aa4c5824
jill64 <143883742+jill64@users.noreply.github.com>
2023-10-12 13:37:27 +0000

    add babel

--- src/build.ts
+++ src/build.ts
@@ -1,5 +1,12 @@
+import { exec } from 'child_process'
 import { build as esbuild } from 'esbuild'
-import { appendFile } from 'fs/promises'
+import { appendFile, mkdir } from 'fs/promises'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { promisify } from 'util'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export const build = async (
   /**
@@ -13,18 +20,31 @@ export const build = async (
    */
   dist: string
 ) => {
+  const tmp = path.join(__dirname, 'tmp', 'index.js')
+
+  const distFull = path.resolve(dist)
+  const distDir = path.dirname(distFull)
+
+  const ready = mkdir(distDir, { recursive: true })
+
   await esbuild({
     bundle: true,
-    target: 'es5',
+    target: 'es6',
     platform: 'neutral',
     entryPoints: [source],
-    outfile: dist,
+    outfile: tmp,
     format: 'iife',
     globalName: 'main'
   })
 
   await appendFile(
-    dist,
+    tmp,
     'function handler (event) { return main.default(event); }'
   )
+
+  await ready
+
+  await promisify(exec)(`npx babel ${tmp} --out-file ${distFull}`, {
+    cwd: __dirname
+  })
 }
