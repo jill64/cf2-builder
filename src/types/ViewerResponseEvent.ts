b0f5682b6216d21c9daa34531f18edc7621270fd
jill64 <intents.turrets0h@icloud.com>
2023-12-29 22:52:45 +0900

    fix: typo

--- src/types/ViewerResponseEvent.ts
+++ src/types/ViewerResponseEvent.ts
@@ -1,8 +1,8 @@
-import { CF2Context } from './filed/CF2Context.js'
-import { CF2IncludeBodyOption } from './filed/CF2IncludeBodyOption.js'
-import { CF2Request } from './filed/CF2Request.js'
-import { CF2Response } from './filed/CF2Response.js'
-import { CF2Viewer } from './filed/CF2Viewer.js'
+import { CF2Context } from './field/CF2Context.js'
+import { CF2IncludeBodyOption } from './field/CF2IncludeBodyOption.js'
+import { CF2Request } from './field/CF2Request.js'
+import { CF2Response } from './field/CF2Response.js'
+import { CF2Viewer } from './field/CF2Viewer.js'
 
 export type ViewerResponseEvent<T extends CF2IncludeBodyOption = undefined> = {
   version: '1.0'
